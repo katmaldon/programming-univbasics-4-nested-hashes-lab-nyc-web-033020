fb5bbea925748dbc3ae1865ed09b0ed721aabd60
Kat Maldon <katharinamaldon@gmail.com>
2020-03-27 01:38:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,12 +61,12 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
+add the following information to the top level of programmer_hash
+:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+return the entire updated hash
 
 	programmer_hash = 
  		{
